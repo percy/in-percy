d030ababcdb4da7575a7988b27b3aabf185b5057
Tim Haines <tmhaines@gmail.com>
2018-01-20 16:17:19 -0800

    Fix window.location matching when rendering in Chrome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Returns true if executing in the Percy rendering environment
 module.exports = function() {
   var hasHostname = typeof window === 'object' && window.location && window.location.hostname;
-  return hasHostname && /proxyme\.percy\.io|renderer\.percy\.local/.test(window.location.hostname);
+  return hasHostname && /proxyme\.percy\.io|render\.percy\.local/.test(window.location.hostname);
 }
